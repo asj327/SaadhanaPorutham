5a7f7d2436e3fdb1ba8d62ae78cd847f1166f98e
Anna <jojianna734@gmail.com>
2026-09-04 07:29:42 +0530

    updated firebase.py

--- backend/firebase.py
+++ backend/firebase.py
@@ -1,7 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
+import base64
 
-cred = credentials.Certificate("firebase-key.json")
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+
+if firebase_credentials:
+    decoded = base64.b64decode(firebase_credentials).decode("utf-8")
+    firebase_config = json.loads(decoded)
+    cred = credentials.Certificate(firebase_config)
+else:
+    cred = credentials.Certificate("firebase-key.json")
 
 firebase_admin.initialize_app(cred)
 
